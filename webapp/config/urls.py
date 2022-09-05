d595c409c4f61e29c28d458b3da17c82791e9e87
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-05 23:51:59 +0900

    chore: index

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http.response import HttpResponse
 from django.urls import include, path
 
+
+def index(request):
+    return HttpResponse('success')
+
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('participants/', include('participant.urls')),
 ]
