e1c714c9d026ee2a6180f0f3734c6e674ee261ee
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-07 23:54:34 +0900

    feat: admin action

--- webapp/participant/admin/participant_admin.py
+++ webapp/participant/admin/participant_admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
-from participant.models import Participant
+from participant.jobs.check_solve_job import perform_check_solve_job
+from participant.models import DailySolveLog, Participant, SolveLog
+
+
+def reset(self, request, queryset):
+    SolveLog.objects.all().delete()
+    DailySolveLog.objects.all().delete()
+    Participant.objects.all().update(failed_days_count=0)
+    perform_check_solve_job('daily')
+    perform_check_solve_job('hour')
+
+
+def refresh_daily(self, request, queryset):
+    perform_check_solve_job('daily')
+
+
+def refresh_hour(self, request, queryset):
+    perform_check_solve_job('hour')
 
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     '''Admin View for Participant'''
+    actions = [reset, refresh_daily, refresh_hour]
 
     list_display = (
         'boj_handle',
